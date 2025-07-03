4a323d8f193b71d764cb215d37a2925f535d0f9b
Alok Singh <140353318+AlokSingh04@users.noreply.github.com>
2025-07-03 19:11:26 +0530

    Time: 0 ms (100%), Space: 10.5 MB (34.57%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-    void func(vector<int>& nums, vector<vector<int>>& res, vector<int> temp, int i){
-        if(i == nums.size()){
-            res.push_back(temp);
-            return;
+    void func(vector<int>& nums, vector<vector<int>>& res, vector<int> temp, int start){
+        res.push_back(temp);
+        
+        for(int i=start; i<nums.size(); i++){
+            temp.push_back(nums[i]);
+            func(nums, res, temp, i+1);
+            temp.pop_back();
         }
-
-        // exclude.
-        func(nums, res, temp, i+1);
-
-        // include.
-        temp.push_back(nums[i]);
-        func(nums, res, temp, i+1);
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
