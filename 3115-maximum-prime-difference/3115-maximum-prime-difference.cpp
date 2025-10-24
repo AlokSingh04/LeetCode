ac3fb33e64ce583c8b478676380aeae5a08e062f
Alok Singh <140353318+AlokSingh04@users.noreply.github.com>
2025-10-24 09:21:33 +0530

    Time: 84 ms (70.37%), Space: 108.5 MB (98.56%) - LeetHub

---
+++ 3115-maximum-prime-difference/3115-maximum-prime-difference.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPrime(int n){
+        if(n<=1) return 0;
+
+        for(int i=2; i*i<=n; i++) if(n%i == 0) return 0;
+
+        return 1;
+    }
+
+    int maximumPrimeDifference(vector<int>& nums) {
+        int i=0, j=nums.size()-1;
+
+        for(i=0;i<nums.size();i++) if(isPrime(nums[i])) break;
+        for(j=nums.size()-1;j>=0;j--) if(isPrime(nums[j])) break;
+        
+        return j-i;
+    }
+};
