19c9af9cdfb230881c35c921aac191d68eaf4af7
Alok Singh <140353318+AlokSingh04@users.noreply.github.com>
2025-10-31 09:46:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,19 +1,45 @@
 class Solution {
 public:
+    // int countPrimes(int n) {
+    //     if(n <= 1) return false;
+
+    //     vector<bool> prime(n,true);
+    //     prime[0] = prime[1] = false;
+
+    //     for(int i=2; i*i<n; i++){
+    //         if(prime[i]){
+    //             for(int j=2*i; j<n; j+=i){
+    //                 prime[j] = false;
+    //             }
+    //         }
+    //     }
+
+    //     return count(prime.begin(), prime.end(), true);
+    // }
+
+
+
     int countPrimes(int n) {
-        if(n <= 1) return false;
+        if(n<=2) return 0;
+        vector<bool> arr(n+1,false);
+        arr[2]=arr[3]=true;
 
-        vector<bool> prime(n,true);
-        prime[0] = prime[1] = false;
-
-        for(int i=2; i<sqrt(n); i++){
-            if(prime[i]){
-                for(int j=i+i; j<n; j+=i){
-                    prime[j] = false;
-                }
+        for(int x = 1; x*x<=n; x++){
+            for(int y=1; y*y<=n; y++){
+                int num = 4*x*x + y*y;
+                if(num<=n && (num%12==1 || num%12==5)) arr[num] = !arr[num];
+                num = 3*x*x + y*y;
+                if(num<=n && num%12==7) arr[num] = !arr[num];
+                num = 3*x*x - y*y;
+                if(x>y && num<=n && num%12==11) arr[num] = !arr[num]; 
             }
         }
 
-        return count(prime.begin(), prime.end(), true);
+        for(int i = 5; i*i<=n; i++){
+            if(!arr[i]) continue;
+            for(int j=i*i; j<=n; j+=i*i) arr[j] = false;
+        }
+
+        return count(arr.begin()+2,arr.end(), true);
     }
 };
