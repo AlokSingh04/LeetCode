28bd3278c3358f042ab20d001325d30797ba4237
Alok Singh <140353318+AlokSingh04@users.noreply.github.com>
2025-11-21 11:25:21 +0530

    Time: 19 ms (14.27%), Space: 19.5 MB (28.21%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int func(vector<vector<int>>& dp, vector<int>& piles, int total, int i, int j){
-        if(i>j) return 0;
+    int solve(vector<vector<int>>& dp, vector<int>& piles, int i, int j) {
+        if (i > j) return 0;
 
-        if(dp[i][j]!=-1) return dp[i][j];
+        if (dp[i][j] != -1) return dp[i][j];
 
-        dp[i][j] = total-min(func(dp,piles,total-piles[i],i+1,j), func(dp,piles,total-piles[j],i,j-1));
+        int left  = piles[i] - solve(dp, piles, i + 1, j);
+        int right = piles[j] - solve(dp, piles, i, j - 1);
 
-        return dp[i][j];
+        return dp[i][j] = max(left, right);
     }
 
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<int>> dp(n, vector<int>(n,-1));
+        vector<vector<int>> dp(n, vector<int>(n, -1));
 
-        int total = 0;
-        for(int x : piles) total += x;
-
-        int res = func(dp,piles,total,0,piles.size()-1);
-
-        return res > total-res;
+        return solve(dp, piles, 0, n - 1) > 0;
     }
 };
