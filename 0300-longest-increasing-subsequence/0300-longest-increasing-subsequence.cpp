7990389e8ffa8509a5a638e9511d9049466957d2
Alok Singh <140353318+AlokSingh04@users.noreply.github.com>
2025-12-15 10:13:20 +0530

    Time: 115 ms (37.74%), Space: 14.5 MB (45.43%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -15,7 +15,7 @@ public:
 
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1,-1);
+        vector<int> dp(n,-1);
         int ans = 0;
         for(int i=0;i<n;i++){
             ans = max(ans, func(dp,i,nums));
